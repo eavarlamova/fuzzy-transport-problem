c5ae37ae64b3e70eb764d0fab8587f6171d1a225
Ekaterina Varlamova <e.varlamova@dunice.net>
2021-04-28 16:19:11 +0300

    render matrix

--- src/pages/FormPage/components/ResaltTable/index.jsx
+++ src/pages/FormPage/components/ResaltTable/index.jsx
@@ -1,24 +1,43 @@
-import { Table, TableHead, TableCell, TableBody } from "@material-ui/core";
+import { Table, TableHead, TableCell, TableBody, TableRow } from "@material-ui/core";
 import { memo } from "react";
 
 const ResultTable = (props) => {
   const {
-    points
+    points,
+    matrix,
   } = props;
 
   return (
     <>
 
       <Table>
-        <TableHead>
-          <TableCell>ПО/ПН</TableCell>
-          {points.destination.map(({ name, quality }) => (
-            <TableCell> {name} - {quality} </TableCell>
-          )) || ''}
-        </TableHead>
-        <TableBody></TableBody>
+        {/* <TableHead> */}
+          <TableRow>
+            <TableCell>ПО/ПН</TableCell>
+            {points.destination.map(({ name, quality }) => (
+              <TableCell> {name} - {quality} </TableCell>
+            )) || ''}
+          </TableRow>
+        {/* </TableHead>
+        <TableBody> */}
+          {console.log('#######', matrix, '#######')}
+          {matrix.map((item, index) => {
+            return (
+              <TableRow>
+                <TableCell> {points.departure[index].name}</TableCell>
+                {item.map(item => <TableCell> {item.x}</TableCell> )}
+                {/* <TableCell> </TableCell> */}
+              </TableRow>
+            )
+          })}
+          {/* <TableRow>
+            <TableCell>
+
+            </TableCell>
+          </TableRow> */}
+        {/* </TableBody> */}
       </Table>
-      {/* 
+      {/*       
       {
         points.destination.map(({ name, quality }) => (
           <li> {name} - {quality} </li>
--- src/pages/FormPage/index.jsx
+++ src/pages/FormPage/index.jsx
@@ -1,5 +1,5 @@
 import { Button, Card, Grid, Input, Typography } from '@material-ui/core';
-import React, { memo, useState, useMemo, useCallback } from 'react';
+import React, { memo, useState, useMemo, useCallback, useEffect } from 'react';
 import AddingForm from "./components/AddingForm";
 import ResaltTable from "./components/ResaltTable";
 
@@ -9,13 +9,18 @@ const FormPage = () => {
   const [points, setPoints] = useState({ departure: [], destination: [] }); // пункты отправления и назначения в виде объектов с полямя name и quality
   const [currentPoint, setCurrentPoint] = useState({ departure: {}, destination: {} });
   const [matrix, setMatrix] = useState([])
-  // const matrix = [
-  //   // [{x: 11 , c: 11}, {x: 12, c: 12}],
-  //   // [{x: 21, c: 21}, {x: 22, c: 22}],
-  //   // [{x: 31, c: 31}, {x: 50, c: null}],
+  // const matrix1 = [
+  //   [{x: 11 , c: 11}, {x: 12, c: 12}],
+  //   [{x: 21, c: 21}, {x: 22, c: 22}],
+  //   [{x: 31, c: 31}, {x: 50, c: null}],
   // ];
+  // useEffect(()=>{ 
+  //   setMatrix(matrix1)
+  // setPoints({ departure: ['ПО 1','ПО 2', 'ПО 3' ], destination: [ 'ПH 1', 'ПH 2', 'ПH 3' ] })
+  // }, [])
+  
 
-  const setMatrixField = useCallback(() => {
+  const setMatrixField = useMemo(() => {
     const { departure, destination } = points;
     const lengthDestination = destination.length; // кол-во ПН
 
@@ -41,6 +46,7 @@ const FormPage = () => {
       }
       return [...acc, arr]
     }, [])
+    console.log('newArr', newArr)
     setMatrix(newArr);
   }, [points])
 
@@ -99,6 +105,7 @@ const FormPage = () => {
         <Grid item xs={12} >
           <ResaltTable
             points={points}
+            matrix={matrix}  
           />
         </Grid>
       </Grid>
