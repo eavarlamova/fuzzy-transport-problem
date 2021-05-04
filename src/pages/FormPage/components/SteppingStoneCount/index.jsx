fb46d5a40926ab726649bf3539d54b1f95daa9eb
Ekaterina Varlamova <e.varlamova@dunice.net>
2021-05-04 19:28:17 +0300

    create manage sunction for futhure recurce

--- src/pages/FormPage/components/SteppingStoneCount/index.jsx
+++ src/pages/FormPage/components/SteppingStoneCount/index.jsx
@@ -55,11 +55,56 @@ const SteppingStoneCount = (props) => {
         return cordinateArray;
       }
     }
-  }
+  };
+
+  const manage = function manageMe(tempMatrix, objOfCoordinat, col) {
+    if (tempMatrix[objOfCoordinat.OY1[0]][objOfCoordinat.OX1[1]].x) {
+      tempMatrix[objOfCoordinat.OY1[0]][objOfCoordinat.OX1[1]].x++;
+    }
+    else {
+      if (objOfCoordinat.OX1[1] < col) {
+
+        // find new OX coordinate
+        objOfCoordinat.OX2 = getNewCoordinatePlus(objOfCoordinat.OX1[1], objOfCoordinat.OY1[1], objOfCoordinat.OY1[0])
+        const [coordinateRowOX2, coordinateColOX2] = objOfCoordinat.OX2
+        tempMatrix[coordinateRowOX2][coordinateColOX2].x++;
+
+        // find new OY coordinat
+        objOfCoordinat.OY2 = getNewCoordinateMinus(objOfCoordinat.OY1[0], objOfCoordinat.OX1[0], objOfCoordinat.OX1[1]);
+        const [coordinateRowOY2, coordinateColOY2] = objOfCoordinat.OY2
+        tempMatrix[coordinateRowOY2][coordinateColOY2].x++;
+      }
+      else {
+        objOfCoordinat.OX2 = getNewCoordinateMinus(
+          objOfCoordinat.OX1[1],
+          objOfCoordinat.OY1[1],
+          objOfCoordinat.OY1[0],
+          'bottom'
+        )
+        const [coordinateRowOX2, coordinateColOX2] = objOfCoordinat.OX2
+        tempMatrix[coordinateRowOX2][coordinateColOX2].x++;
+
+        // find new OY coordinat
+        const newOY = getNewCoordinateMinus(
+          objOfCoordinat.OX1[0] - 1,
+          objOfCoordinat.OY1[0] + 1,
+          objOfCoordinat.OX1[1]
+        )
+        if (newOY) {
+          objOfCoordinat.OY2 = newOY;
+          const [coordinateRowOY2, coordinateColOY2] = objOfCoordinat.OY2
+          tempMatrix[coordinateRowOY2][coordinateColOY2].x++;
+        }
+      }
+      // поиск доп непустых клеток куда добавить +1 для уравнивания столбцов и строк
+    }
+    
+    console.log('tempMatrix', tempMatrix)
+   };
 
 
   const kek = () => {
-    let findCol = null;
+    // let findCol = null;
 
     const rowCount = matrix.length;
     const colCount = matrix[0].length;
@@ -73,8 +118,8 @@ const SteppingStoneCount = (props) => {
         let currentCell = currentRow[col];
         haveFullX = haveFullX || Boolean(currentCell.x);
 
-        let indexOX = null;
-        let indexOY = null;
+        // let indexOX = null;
+        // let indexOY = null;
         const objOfCoordinat = {};
         if (currentCell.x === 0) {
           // мы нашли пустую клетку
@@ -86,7 +131,7 @@ const SteppingStoneCount = (props) => {
             for (let leftCol = col - 1; leftCol >= 0; leftCol--) {
               if (matrix[row][leftCol].x !== 0) {
                 tempMatrix[row][leftCol].x--;
-                indexOX = leftCol;
+                // indexOX = leftCol;
                 objOfCoordinat.OX1 = [row, leftCol];
                 break;
               }
@@ -95,7 +140,7 @@ const SteppingStoneCount = (props) => {
             for (let bottomRow = row + 1; bottomRow < rowCount; bottomRow++) {
               if (matrix[bottomRow][col].x) {
                 tempMatrix[bottomRow][col].x--;
-                indexOY = bottomRow;
+                // indexOY = bottomRow;
                 objOfCoordinat.OY1 = [bottomRow, col];
                 break;
               }
@@ -106,7 +151,7 @@ const SteppingStoneCount = (props) => {
             for (let rigthCol = col + 1; rigthCol < colCount; rigthCol++) {
               if (matrix[row][rigthCol].x !== 0) {
                 tempMatrix[row][rigthCol].x--;
-                indexOX = rigthCol;
+                // indexOX = rigthCol;
                 objOfCoordinat.OX1 = [row, rigthCol];
                 break;
               }
@@ -116,54 +161,56 @@ const SteppingStoneCount = (props) => {
             for (let topRow = row - 1; topRow >= 0; topRow--) {
               if (matrix[topRow][col].x) {
                 tempMatrix[topRow][col].x--;
-                indexOY = topRow;
+                // indexOY = topRow;
                 objOfCoordinat.OY1 = [topRow, col];
                 break;
               }
             }
           }
 
-          if (tempMatrix[indexOY][indexOX].x) {
-            tempMatrix[indexOY][indexOX].x++;
-          }
-          else {
-            if (indexOX < col) {
+          // replace manage();
+          manage(tempMatrix, objOfCoordinat, col)
+          // if (tempMatrix[objOfCoordinat.OY1[0]][objOfCoordinat.OX1[1]].x) {
+          //   tempMatrix[objOfCoordinat.OY1[0]][objOfCoordinat.OX1[1]].x++;
+          // }
+          // else {
+          //   if (objOfCoordinat.OX1[1] < col) {
 
-              // find new OX coordinate
-              objOfCoordinat.OX2 = getNewCoordinatePlus(objOfCoordinat.OX1[1], objOfCoordinat.OY1[1], objOfCoordinat.OY1[0])
-              const [coordinateRowOX2, coordinateColOX2] = objOfCoordinat.OX2
-              tempMatrix[coordinateRowOX2][coordinateColOX2].x++;
+          //     // find new OX coordinate
+          //     objOfCoordinat.OX2 = getNewCoordinatePlus(objOfCoordinat.OX1[1], objOfCoordinat.OY1[1], objOfCoordinat.OY1[0])
+          //     const [coordinateRowOX2, coordinateColOX2] = objOfCoordinat.OX2
+          //     tempMatrix[coordinateRowOX2][coordinateColOX2].x++;
 
-              // find new OY coordinat
-              objOfCoordinat.OY2 = getNewCoordinateMinus(objOfCoordinat.OY1[0], objOfCoordinat.OX1[0], objOfCoordinat.OX1[1]);
-              const [coordinateRowOY2, coordinateColOY2] = objOfCoordinat.OY2
-              tempMatrix[coordinateRowOY2][coordinateColOY2].x++;
-            }
-            else {
-              objOfCoordinat.OX2 = getNewCoordinateMinus(
-                objOfCoordinat.OX1[1],
-                objOfCoordinat.OY1[1],
-                objOfCoordinat.OY1[0],
-                'bottom'
-              )
-              const [coordinateRowOX2, coordinateColOX2] = objOfCoordinat.OX2
-              tempMatrix[coordinateRowOX2][coordinateColOX2].x++;
+          //     // find new OY coordinat
+          //     objOfCoordinat.OY2 = getNewCoordinateMinus(objOfCoordinat.OY1[0], objOfCoordinat.OX1[0], objOfCoordinat.OX1[1]);
+          //     const [coordinateRowOY2, coordinateColOY2] = objOfCoordinat.OY2
+          //     tempMatrix[coordinateRowOY2][coordinateColOY2].x++;
+          //   }
+          //   else {
+          //     objOfCoordinat.OX2 = getNewCoordinateMinus(
+          //       objOfCoordinat.OX1[1],
+          //       objOfCoordinat.OY1[1],
+          //       objOfCoordinat.OY1[0],
+          //       'bottom'
+          //     )
+          //     const [coordinateRowOX2, coordinateColOX2] = objOfCoordinat.OX2
+          //     tempMatrix[coordinateRowOX2][coordinateColOX2].x++;
 
-              // find new OY coordinat
-              const newOY = getNewCoordinateMinus(
-                objOfCoordinat.OX1[0] - 1,
-                objOfCoordinat.OY1[0] + 1,
-                objOfCoordinat.OX1[1]
-              )
-              if (newOY) {
-                objOfCoordinat.OY2 = newOY;
-                const [coordinateRowOY2, coordinateColOY2] = objOfCoordinat.OY2
-                tempMatrix[coordinateRowOY2][coordinateColOY2].x++;
-              }
-            }
-            // поиск доп непустых клеток куда добавить +1 для уравнивания столбцов и строк
-          }
-          console.log('#######', tempMatrix, '#######')
+          //     // find new OY coordinat
+          //     const newOY = getNewCoordinateMinus(
+          //       objOfCoordinat.OX1[0] - 1,
+          //       objOfCoordinat.OY1[0] + 1,
+          //       objOfCoordinat.OX1[1]
+          //     )
+          //     if (newOY) {
+          //       objOfCoordinat.OY2 = newOY;
+          //       const [coordinateRowOY2, coordinateColOY2] = objOfCoordinat.OY2
+          //       tempMatrix[coordinateRowOY2][coordinateColOY2].x++;
+          //     }
+          //   }
+          //   // поиск доп непустых клеток куда добавить +1 для уравнивания столбцов и строк
+          // }
+          // console.log('#######', tempMatrix, '#######')
         }
         tempMatrix = getDeepClone();
         // обнулить темпМатрикс
